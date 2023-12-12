0510861e9194eb1425ed4fc0d0a50076cf465dbf
wkv816 <wkv816@haw-hamburg.de>
2023-12-12 16:12:37 +0100

    abgabe stand

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,8 @@
 #include <unordered_map>
 #include <vector>
 
+#include <map>
+
 // CAF includes
 #include "caf/all.hpp"
 #include "caf/io/all.hpp"
@@ -88,14 +90,14 @@ void printWorkersendMessage(int512_t result, double cpu_time_diff) {
 
 void printFinalList(vector<int512_t>& list,
                     std::chrono::steady_clock::time_point& start,
-                    double &cpu_time, bool& isprinted) {
+                    double& cpu_time, bool& isprinted,int512_t n) {
   isprinted = true;
   double task_time_end = deftime(start);
   std::cout << "Elapsed Task time: " << task_time_end << " seconds"
             << std::endl;
-  std::cout << "Cpu time diff: " << cpu_time << " seconds" << std::endl;
+  std::cout << "Cpu time diff: " << std::fixed << cpu_time << " seconds" << std::endl;
 
-  std::string printlist = "Prime list of = { ";
+  std::string printlist = "Prime factorial of " + to_string(n) + " = { ";
   for (auto i : list) {
     printlist += to_string(i) + ", ";
   }
@@ -121,12 +123,11 @@ bool isPrime(int512_t n) {
   return true;
 }
 
-
 int512_t generate_Random_Nr(int512_t max, int512_t min) {
-std::random_device rd;
-std::mt19937_64 generator(rd());
-mp::uniform_int_distribution<int512_t> distribution(min, max);
-return distribution(generator);
+  std::random_device rd;
+  std::mt19937_64 generator(rd());
+  mp::uniform_int_distribution<int512_t> distribution(min, max);
+  return distribution(generator);
 }
 /* int512_t generate_Random_Nr(int512_t max, int512_t min) {
   // Seed the random number generator
@@ -139,25 +140,26 @@ return distribution(generator);
   // Generate a random number
   return distribution(generator);
 } */
-int512_t pollard_rho(int512_t n, int512_t zufall) {
+int512_t pollard_rho(int512_t n, int512_t zufall, int512_t x) {
   // TODO: Implement me.
   // Set the duration to run the while loop
   const std::chrono::seconds duration(5);
   auto start_time = std::chrono::steady_clock::now();
 
-  int512_t x = generate_Random_Nr(n,1);
-  cout << "generate_Random_Nr = " << x << std::endl;
   int512_t y = x;
   int512_t p = 1;
-
+  int512_t i = 0;
   while (p == 1 || p == n) {
-    x = (x * x + 1) % n;
-    y = (y * y + 1) % n;
-    y = (y * y + 1) % n;
+    i++;
+
+    x = (x * x + zufall) % n;
+    y = (y * y + zufall) % n;
+    y = (y * y + zufall) % n;
     p = mp::gcd(mp::abs(x - y), n);
 
     // Check if the loop has finished if not exit after duration
     if (std::chrono::steady_clock::now() - start_time >= duration) {
+      printMessage("i = " + to_string(i));
       // printMessage("Loop finished after 5 seconds.");
       return -1; // Exit the loop
     }
@@ -209,14 +211,24 @@ behavior client(stateful_actor<client_state>* self, caf::group grp) {
   // TODO: Implement me.
 
   int512_t a = 3;
-  int512_t n("1000602106143806596478722974273666950903906112131794745457338659266842446985022076792112309173975243506969710503");
-  //int512_t n("1137047281562824484226171575219374004320812483047");
-  //int512_t n(8806715679);
+  
+  // int512_t n(8806715679);
+  // int512_t n("9398726230209357241");
+  // int512_t n("1137047281562824484226171575219374004320812483047");
+  //int512_t n("1000602106143806596478722974273666950903906112131794745457338659266842446985022076792112309173975243506969710503");
+  int512_t n("607222445970228420877428470856114255886552745995590959538783047195414848136116951978042939979743");
 
+  if(is_probable_prime(n)){
+    cout << "the number is prime" << std::endl;
+    return {};
+  }
+
+
+  int512_t finaln=n;
 
   self->state.task_start_time = high_resolution_clock::now();
   self->state.tasklist.push_back(n);
-  self->send(grp, "worker_atom_v", n, a, n,0.0);
+  self->send(grp, "worker_atom_v", n, a, n, 0.0);
   cout << "behavior client started " << std::endl;
 
   return {
@@ -234,15 +246,15 @@ behavior client(stateful_actor<client_state>* self, caf::group grp) {
 
         cout << "the factorial of " << urN << " = " << p << " und  " << ndurchP
              << std::endl;
+        self->send<message_priority::high>(grp, "Abbruch", urN, p, ndurchP);
         if (is_probable_prime(p)) {
           cout << "++++ Primzahl " << p << " ++++" << std::endl;
           self->state.fact_list.push_back(p);
 
         } else {
           self->state.tasklist.push_back(p);
-          self->send(grp, "worker_atom_v", p, a, n,0.0);
+          self->send(grp, "worker_atom_v", p, a, n, 0.0);
         }
-        
 
         if (is_probable_prime(ndurchP)) {
           cout << "++++ Primzahl " << ndurchP << " ++++" << std::endl;
@@ -250,14 +262,23 @@ behavior client(stateful_actor<client_state>* self, caf::group grp) {
 
         } else {
           self->state.tasklist.push_back(ndurchP);
-          self->send(self->state.grp, "worker_atom_v", ndurchP, a, n,0.0);
+          self->send(self->state.grp, "worker_atom_v", ndurchP, a, n, 0.0);
         }
       }
 
       if (self->state.tasklist.empty() && self->state.isprinted == false) {
         printFinalList(self->state.fact_list, self->state.task_start_time,
-                       self->state.cpu_time_total, self->state.isprinted);
+                       self->state.cpu_time_total, self->state.isprinted,finaln);
       }
+    },
+    [=](const std::string& message, int512_t n, int512_t p, int512_t ndurchP) {
+    },
+    [=](bool bol) {
+      if(self->state.tasklist.empty() == false){
+        int512_t currentTask = self->state.tasklist.front();
+        self->send(grp, "worker_atom_v", currentTask, a, n, 0.0);
+      }
+      
     }};
   return {};
 }
@@ -279,12 +300,15 @@ void run_client(actor_system& sys, const config& cfg) {
 struct worker_state {
   // The joined group.
   group grp;
+  vector<int512_t> oldTasklist;
+  std::map<int512_t, vector<int512_t>> oldTaskMap;
 };
 
 behavior worker(stateful_actor<worker_state>* self, caf::group grp) {
   // Join group and save it to send messages later.
   self->join(grp);
   self->state.grp = grp;
+  self->send(grp,true);
 
   printMessage("behavior worker has been started");
 
@@ -294,21 +318,48 @@ behavior worker(stateful_actor<worker_state>* self, caf::group grp) {
     [=](const std::string& message, int512_t n, int512_t zufall, int512_t z,
         double cpu_time) {
       if (message == "worker_atom_v") {
+        int loopStep = 0;
         // calculate cpu time abd do pollard rho algorithm
         auto cpu_time_start = high_resolution_clock::now();
-
-        int512_t result = pollard_rho(n, zufall);
-
-        double cpu_time_diff = deftime(cpu_time_start);
-
-        if (result != -1) {
-          self->send(grp, "client_atom_v", result, n / result, n,
+        double cpu_time_diff;
+        if (self->state.oldTaskMap.find(n) != self->state.oldTaskMap.end()) {
+          cpu_time_diff = deftime(cpu_time_start);
+          self->send(self->state.grp, "client_atom_v", n,
+                     self->state.oldTaskMap[n][0], self->state.oldTaskMap[n][1],
                      cpu_time_diff);
-          printWorkersendMessage(result, cpu_time_diff);
+          return;
+        }
+
+        int512_t a = 1;
+        int512_t randamnr = generate_Random_Nr(n, 1);
+        while (true) {
+          int512_t result = pollard_rho(n, a, randamnr);
+          cpu_time_diff = deftime(cpu_time_start);
+          if (result != -1) {
+            printMessage("a = " + to_string(a));
+            self->send(grp, "client_atom_v", result, n / result, n,
+                       cpu_time_diff);
+            printWorkersendMessage(result, cpu_time_diff);
+            return;
+          } else if (self->mailbox().empty() == false) {
+          }
+
+          a = a + 1;
+          loopStep++;
         }
       }
     },
-  };
+    [=](const std::string& message, int512_t n, int512_t p, int512_t ndurchP) {
+      if (message == "Abbruch") {
+        // Daten in die Map eintragen
+        self->state.oldTaskMap[n].push_back(p);
+        self->state.oldTaskMap[n].push_back(ndurchP);
+
+        // self->state.grp = grp;
+      }
+    },
+    [=](bool bol) {
+    }};
 
   // TODO: Implement me.
   // - Calculate rho.
